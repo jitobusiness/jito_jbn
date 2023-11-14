90dc42a4dcbbb11f2797daf0a81a1f4907559bd7
jitobusiness <127860971+jitobusiness@users.noreply.github.com>
2023-11-14 14:01:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -497,7 +497,7 @@ def results():
             context_parameter_name = ['chapter_id']
             context_value = [chapter_id]
             
-            text = "Please tell me the *group name* in which you want to post your enquiry."
+            text = "Please tell me the *industry group name* in which you want to post your enquiry."
             return return_text_with_context(text,context_session,context_parameter_name,context_value)
             
         if len(req['queryResult']['parameters']['sell_message'])<1:
